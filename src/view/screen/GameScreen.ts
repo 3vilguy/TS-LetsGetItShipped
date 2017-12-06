47ee39a1aa9251ce93d0e105bc82ba4a86e723fa
3vilguy <3vilguy@gmail.com>
2017-12-06 14:31:28 +0000

    Picking random key to mash.

--- src/view/screen/GameScreen.ts
+++ src/view/screen/GameScreen.ts
@@ -63,7 +63,14 @@ export default class GameScreen extends Container {
     }
 
     private startMashing() {
-        this.randKey = "KeyL";
+        var allKeys = [
+            "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+            "A", "S", "D", "F", "G", "H" ,"J", "K", "L",
+            "Z", "X", "C", "V", "B", "N", "M"
+        ];
+        var randKey = allKeys[Math.floor(Math.random()*allKeys.length)];
+        console.log("RANDOM KEY => " + randKey);
+        this.randKey = "Key" + randKey;
         this.count = 0;
         window.addEventListener('keyup', this.handleKeyMashing);
     }
