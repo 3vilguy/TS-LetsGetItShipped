218a1c62d6922ae825b7e203f098f37c6a7c0409
3vilguy <3vilguy@gmail.com>
2017-12-19 20:13:43 +0100

    Remove TaskPicker (at this point it's just text on top anyway).

--- src/view/screen/GameScreen.ts
+++ src/view/screen/GameScreen.ts
@@ -1,12 +1,10 @@
 import { Container } from 'pixi.js';
 
-import TaskPicker from '../component/TaskPicker';
 import Desk from '../component/Desk';
 import { WIDTH, HEIGHT } from '../../constants/RendererConstants';
 
 export default class GameScreen extends Container {
     private desk : Desk;
-    private taskPicker : TaskPicker;
 
     constructor() {
         super();
@@ -17,20 +15,14 @@ export default class GameScreen extends Container {
         this.desk = new Desk();
         this.desk.x = (WIDTH - this.desk.width) * 0.5;
         this.desk.y = HEIGHT * 0.7;
-
-        this.taskPicker = new TaskPicker();
-        this.taskPicker.x = (WIDTH - this.taskPicker.width) * 0.5;
     }
 
     public start() {
         this.addChild(this.desk);
-        this.addChild(this.taskPicker);
-
         this.desk.showTasks();
     }
 
     public removeTaskPicker() {
-        this.removeChild(this.taskPicker);
         this.desk.removeTasks();
     }
 }
