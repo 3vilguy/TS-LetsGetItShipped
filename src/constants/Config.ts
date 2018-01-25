85f89c88a0ca0c40680f710226c98d336ef2da40
3vilguy <3vilguy@gmail.com>
2018-01-25 09:40:55 +0000

    Add animation in Extras layer. Joe and Mensah would randomly peak over the monitor every now and then. Moving some animation related value to config, so it's easier to change them.

---
+++ src/constants/Config.ts
@@ -0,0 +1,5 @@
+// EXTRAS LAYER - JOE & MENSAH
+export const HEAD_UP_ANIMATION_TIME_SEC : number = 1;
+export const HEAD_UP_TIME_SEC : number = 3;
+export const HEAD_DOWN_ANIMATION_TIME_SEC : number = 1;
+export const DELAY_BETWEEN_ANIMATIONS_SEC : number = 6;
--- src/view/component/Extras.ts
+++ src/view/component/Extras.ts
@@ -1,5 +1,12 @@
 import { Container, Sprite } from 'pixi.js';
+import { TweenLite, TimelineLite } from 'gsap';
 
+import {
+    HEAD_UP_ANIMATION_TIME_SEC,
+    HEAD_UP_TIME_SEC,
+    HEAD_DOWN_ANIMATION_TIME_SEC,
+    DELAY_BETWEEN_ANIMATIONS_SEC
+} from '../../constants/Config';
 import { WIDTH, HEIGHT } from '../../constants/RendererConstants';
 
 export default class Extras extends Container {
@@ -14,13 +21,33 @@ export default class Extras extends Container {
     private init() {
         var yPos:number = HEIGHT * 0.47;
 
-        this.mensah = new PIXI.Sprite(PIXI.loader.resources[require('../../../assets/images/extras/Mensah.png')].texture);
+        this.mensah = new Sprite(PIXI.loader.resources[require('../../../assets/images/extras/Mensah.png')].texture);
         this.mensah.y = yPos;
         this.addChild(this.mensah);
 
-        this.joe = new PIXI.Sprite(PIXI.loader.resources[require('../../../assets/images/extras/Joe.png')].texture);
+        this.joe = new Sprite(PIXI.loader.resources[require('../../../assets/images/extras/Joe.png')].texture);
         this.joe.x = WIDTH * 0.63;
         this.joe.y = yPos;
         this.addChild(this.joe);
+
+        TweenLite.delayedCall(2, this.startTween);
+    }
+
+    private startTween = () => {
+        var sprite:Sprite;
+        if(Math.random() < 0.5) {
+            sprite = this.mensah;
+        } else {
+            sprite = this.joe;
+        }
+        var startY:number = sprite.y;
+
+        var tl = new TimelineLite( {onComplete: this.onTweenFinished} );
+        tl.add( TweenLite.to(sprite, HEAD_UP_ANIMATION_TIME_SEC, {y: startY - 150}) );
+        tl.add( TweenLite.to(sprite, HEAD_DOWN_ANIMATION_TIME_SEC, {y: startY, delay: HEAD_UP_TIME_SEC}) );
+    }
+
+    private onTweenFinished = () => {
+        TweenLite.delayedCall(DELAY_BETWEEN_ANIMATIONS_SEC, this.startTween);
     }
 }
